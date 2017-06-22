ccb834031a4359e78ad90ef937bba9f01149eff7
Chan Y. Park <chan.splendid.park@gmail.com>
2017-06-22 12:21:52 -0400

    implemented discriminator; generator now fully transposed-convolutional.

--- config.py
+++ config.py
@@ -1,13 +1,22 @@
 dcgan_mnist = {
     'generator': [
-        ('input', {'dim': 100}),
-        ('fc', {'out_size': 7, 'out_chs': 128}),
+#        ('input', {'dim': 100}),
+#        ('fc', {'out_size': 7, 'out_chs': 128}),
+#        ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 64}),
+#        ('conv2_output', {'filter_size': 5, 'stride': 2, 'out_chs': 1}),
+        ('conv0_input',
+            {'input_dim': 100, 'filter_size': 7, 'stride': 1, 'out_chs': 128}
+        ),
         ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 64}),
         ('conv2_output', {'filter_size': 5, 'stride': 2, 'out_chs': 1}),
     ],
     'discriminator': [
-        ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 64}),
-        ('conv2', {'filter_size': 5, 'stride': 2, 'out_chs': 128}),
-        ('fc', {}),
+#        ('input', {'size': 28, 'num_chs': 1}),
+        ('conv0_input',
+            {'input_size', 28, 'in_chs': 1,
+             'filter_size': 5, 'stride': 2, 'out_chs': 64}
+        ),
+        ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 128}),
+        ('conv2_output', {'filter_size': 7, 'stride': 1, 'out_chs': 1}),
     ],
 }
--- model.py
+++ model.py
@@ -4,7 +4,7 @@ class DCGAN:
     def __init__(
         self,
         config=None,
-        name=None,
+        training=None,
         log_dir='logs',
         checkpoint_dir='checkpoints',
         gpu_memory_fraction=None,
@@ -14,6 +14,11 @@ class DCGAN:
             os.makedirs(log_dir)
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
+
+        if training is None:
+            raise ValueError('Set training either to be True or False.')
+        else:
+            self.training = training
 
         self._tf_summary = {}
         self._tf_session = None
@@ -37,74 +42,86 @@ class DCGAN:
             self._tf_session = tf.Session(config=self._tf_config)
             self._tf_session.run(tf.global_variables_initializer())
 
-    def _build_generator_network(self):
+    def _build_generator_network(self, minibatch_size=None):
         conf = self._config['generator']
-        prev_layer = tf.placeholder(
-            dtype=tf.float32,
-            shape=(None, conf['input']['dim']),
-            name='Z',
-        )
+        prev_layer = None
 
         for layer_name, layer_conf in conf:
             with tf.variable_scope(layer_name):
-                if 'fc' in layer_name:
-                    minibatch_size, in_chs = prev_layer.shape.as_list() 
-                    out_size = layer_conf['out_size']
-                    out_chs = layer_conf['out_chs']
+#                if 'input' in layer_name:
+#                    new_layer = tf.placeholder(
+#                        dtype=tf.float32,
+#                        shape=(None, conf['input']['dim']),
+#                        name='Z',
+#                    )
+#                elif 'fc' in layer_name:
+#                    minibatch_size, in_chs = prev_layer.shape.as_list() 
+#                    out_size = layer_conf['out_size']
+#                    out_chs = layer_conf['out_chs']
+#
+#                    W = tf.get_variable(
+#                        name='W',
+#                        shape=(in_chs, out_size * out_size * out_chs),
+#                        initializer=self._get_variable_initializer(),
+#                    )
+#
+#                    pre_activation = tf.reshape(
+#                        tf.nn.matmul(prev_layer, W),
+#                        shape=(minibatch_size, out_size, out_size, out_chs),
+#                        name='pre_activation',
+#                    )
+#
+#                    new_layer = tf.reshape(
+#                        activation,
+#                        shape=(minibatch_size, out_size, out_size, out_chs),
+#                        name='output',
+#                    )
+#                elif 'conv' in layer_name:
 
-                    W = tf.get_variable(
-                        name='W',
-                        shape=(in_chs, out_size * out_size * out_chs),
-                        initializer=self._get_variable_initializer(),
+                filter_size = layer_conf['filter_size']
+                stride = layer_conf['stride']
+                out_chs = layer_conf['out_chs']
+                
+
+                if 'input' in layer_name:
+                    in_chs = conf['input_dim']
+                    prev_layer = tf.placeholder(
+                        dtype=tf.float32,
+                        shape=(minibatch_size, 1, 1, in_chs),
+                        name='Z',
                     )
-
-                    pre_activation = tf.reshape(
-                        tf.nn.matmul(prev_layer, W),
-                        shape=(minibatch_size, out_size, out_size, out_chs),
-                        name='pre_activation',
-                    )
-
-                    new_layer = tf.reshape(
-                        activation,
-                        shape=(minibatch_size, out_size, out_size, out_chs),
-                        name='output',
-                    )
-                elif 'conv' in layer_name:
-                    (minibatch_size,
-                     in_size,
-                     _,
-                     in_chs) = prev_layer.shape.as_list()
-
-                    filter_size = layer_conf['filter_size']
-                    stride = layer_conf['stride']
-                    out_chs = layer_conf['out_chs']
-                    
+                    out_size = filter_size
+                else:
+                    _, _, in_size, in_chs = prev_layer.shape.as_list()
                     out_size =  in_size * stride
 
-                    W = tf.get_variable(
-                        name='W',
-                        shape=(filter_size,
-                               filter_size,
-                               in_chs,
-                               out_chs),
-                        initializer=self._get_variable_initializer(),
-                    )
+                W = tf.get_variable(
+                    name='W',
+                    shape=(filter_size,
+                           filter_size,
+                           in_chs,
+                           out_chs),
+                    initializer=self._get_variable_initializer(),
+                )
 
-                    pre_activation = tf.nn.conv2d_transpose(
-                        prev_layer,
-                        W,
-                        output_shape=(minibatch_size,
-                                      out_size,
-                                      out_size,
-                                      out_chs),
-                        strides=(1, stride, stride, 1),
-                        padding='SAME',
-                        name='pre_activation',
-                    )
+                pre_activation = tf.nn.conv2d_transpose(
+                    prev_layer,
+                    W,
+                    output_shape=(minibatch_size,
+                                  out_size,
+                                  out_size,
+                                  out_chs),
+                    strides=(1, stride, stride, 1),
+                    padding='SAME',
+                    name='pre_activation',
+                )
 
                 if 'output' not in layer_name:
                     new_layer = tf.nn.relu(
-                        batch_normalization(input_tensor=pre_activation),
+                        batch_normalization(
+                            input_tensor=pre_activation,
+                            training=self.training,
+                        ),
                         name='activation',
                     )
                 else:
@@ -116,8 +133,64 @@ class DCGAN:
 
         # End of conf for loop.
 
-    def _build_discriminator_network(self):
-        pass
+    def _build_discriminator_network(self, minibatch_size=None):
+        conf = self._config['discriminator']
+        prev_layer = None
+
+        for layer_name, layer_conf in conf:
+            with tf.variable_scope(layer_name):
+                if 'input' in layer_name:
+                    input_size = conf['input_size']
+                    in_chs = conf['in_chs']
+                    prev_layer = tf.placeholder(
+                        dtype=tf.float32,
+                        shape=(minibatch_size, input_size, input_size, in_chs),
+                        name='input',
+                    )
+                else:
+                    _, _, _, in_chs = prev_layer.shape.as_list()
+
+                filter_size = layer_conf['filter_size']
+                stride = layer_conf['stride']
+                out_chs = layer_conf['out_chs']
+
+                W = tf.get_variable(
+                    name='W',
+                    shape=(filter_size,
+                           filter_size,
+                           in_chs,
+                           out_chs),
+                    initializer=self._get_variable_initializer(),
+                )
+
+                pre_activation = tf.nn.conv2d(
+                    prev_layer,
+                    W,
+                    strides=(1, stride, stride, 1),
+                    padding='SAME',
+                    name='pre_activation',
+                )
+
+                if 'input' in layer_name:
+                    new_layer = leaky_relu(pre_activation)
+                elif 'output' not in layer_name:
+                    new_layer = leaky_relu(
+                        batch_normalization(
+                            input_tensor=pre_activation,
+                            training=self.training,
+                        ),
+                    )
+                else:
+                    new_layer = tf.reshape(
+                        tf.sigmoid(pre_activation),
+                        [],
+                        name='output',
+                    )
+
+            # End of layer_name variable scope.
+
+            prev_layer = new_layer
+
 
 
 def batch_normalization(
@@ -210,3 +283,10 @@ def batch_normalization(
 
 def get_exponential_moving_average_op(v_in, v_avg, decay):
     return tf.assign(v_avg, v_avg * decay + v_in * (1 - decay))
+
+def leaky_relu(x, alpha=0.2):
+    return tf.maximum(
+        -x * alpha,
+        x,
+        name='leaky_relu',
+    )
