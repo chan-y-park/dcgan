2ee38452fae44c060bfa3bc8ca9d52b2019617f3
Chan Y. Park <chan.splendid.park@gmail.com>
2017-06-23 12:39:46 -0400

    training implemented

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 dcgan_mnist = {
+    'dataset': 'MNIST',
     'generator': [
-#        ('input', {'dim': 100}),
 #        ('fc', {'out_size': 7, 'out_chs': 128}),
 #        ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 64}),
 #        ('conv2_output', {'filter_size': 5, 'stride': 2, 'out_chs': 1}),
         ('conv0_input',
-            {'input_dim': 100, 'filter_size': 7, 'stride': 1, 'out_chs': 128}
+            {'filter_size': 7, 'stride': 1, 'out_chs': 128}
         ),
         ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 64}),
         ('conv2_output', {'filter_size': 5, 'stride': 2, 'out_chs': 1}),
@@ -13,10 +13,19 @@ dcgan_mnist = {
     'discriminator': [
 #        ('input', {'size': 28, 'num_chs': 1}),
         ('conv0_input',
-            {'input_size', 28, 'in_chs': 1,
-             'filter_size': 5, 'stride': 2, 'out_chs': 64}
+            {'filter_size': 5, 'stride': 2, 'out_chs': 64}
         ),
         ('conv1', {'filter_size': 5, 'stride': 2, 'out_chs': 128}),
         ('conv2_output', {'filter_size': 7, 'stride': 1, 'out_chs': 1}),
     ],
+    'leaky_relu_alpha': 0.2,
+    'input_size': 28,
+    'num_input_chs': 1,
+    'minibatch_size': 128,
+    'adam': {
+        'learning_rate': 0.0002,
+        'beta1': 0.5,
+    },
+    'num_training_iterations': 100,
+    'generator_input': {'low': -1.0, 'high': 1.0, 'size': 100},
 }
--- model.py
+++ model.py
@@ -1,3 +1,4 @@
+import numpy as np
 import tensorflow as tf
 
 class DCGAN:
@@ -14,6 +15,9 @@ class DCGAN:
             os.makedirs(log_dir)
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
+
+        self._config = config
+        self._load_data()
 
         if training is None:
             raise ValueError('Set training either to be True or False.')
@@ -36,17 +40,81 @@ class DCGAN:
                 self._build_generator_network()
             with tf.variable_scope('discriminator'):
                 self._build_discriminator_network()
-            with tf.variable_scope('summary'):
-                self._build_summary_ops()
+
+            if self.training:
+                with tf.variable('train'):
+                    self._build_train_ops()
+
+                with tf.variable_scope('summary'):
+                    self._build_summary_ops()
 
             self._tf_session = tf.Session(config=self._tf_config)
             self._tf_session.run(tf.global_variables_initializer())
 
-    def _build_generator_network(self, minibatch_size=None):
-        conf = self._config['generator']
+    def train(self):
+        if not self.training:
+            raise RuntimeError
+
+        minibatch_size = self._config['minibatch_size']
+
+        for i in range(self._config['num_training_iterations']):
+            real_inputs = self.get_samples_from_data(minibatch_size)
+            fake_inputs = self.generate_samples(minibatch_size)
+
+            fetches = [
+                self._tf_graph.get_tensor_by_name(
+                    'training/real_D_loss:0',
+                ),
+                self._tf_graph.get_tensor_by_name(
+                    'training/fake_D_loss:0',
+                ),
+                self._tf_graph.get_operation_by_name(
+                    'train/minimize_D_loss',
+                ),
+            ]
+
+            feed_dict = {
+                self._tf_graph.get_tensor_by_name(
+                    'training/real_D_logits:0',
+                ): self.get_D_logits(real_inputs),
+                self._tf_graph.get_tensor_by_name(
+                    'training/fake_D_logits:0',
+                ): self.get_D_logits(fake_inputs),
+            }
+
+            real_D_loss, fake_D_loss, _ = self._tf_session.run(
+                fetches=fetches,
+                feed_dict=feed_dict,
+            )
+
+            fetches = [
+                self._tf_graph.get_tensor_by_name(
+                    'training/fake_D_logits:0',
+                ),
+                self._tf_graph.get_tensor_by_name(
+                    'training/G_loss:0',
+                ),
+                self._tf_graph.get_operation_by_name(
+                    'train/minimize_G_loss',
+                ),
+            ]
+
+            feed_dict = {
+                self._tf_graph.get_tensor_by_name(
+                    'training/fake_D_logits:0',
+                ): self.get_D_logits(fake_inputs),
+            }
+
+            G_loss, _ = self._tf_session.run(
+                fetches=fetches,
+                feed_dict=feed_dict,
+            )
+
+    def _build_generator_network(self):
+        minibatch_size = self._config['minibatch_size']
         prev_layer = None
 
-        for layer_name, layer_conf in conf:
+        for layer_name, layer_conf in self._config['generator']:
             with tf.variable_scope(layer_name):
 #                if 'input' in layer_name:
 #                    new_layer = tf.placeholder(
@@ -81,10 +149,10 @@ class DCGAN:
                 filter_size = layer_conf['filter_size']
                 stride = layer_conf['stride']
                 out_chs = layer_conf['out_chs']
-                
 
                 if 'input' in layer_name:
-                    in_chs = conf['input_dim']
+#                    in_chs = layer_conf['input_dim']
+                    in_chs = self._config['generator_input']['size']
                     prev_layer = tf.placeholder(
                         dtype=tf.float32,
                         shape=(minibatch_size, 1, 1, in_chs),
@@ -133,15 +201,15 @@ class DCGAN:
 
         # End of conf for loop.
 
-    def _build_discriminator_network(self, minibatch_size=None):
-        conf = self._config['discriminator']
+    def _build_discriminator_network(self):
+        minibatch_size = self._config['minibatch_size']
         prev_layer = None
 
-        for layer_name, layer_conf in conf:
+        for layer_name, layer_conf in self._config['discriminator']:
             with tf.variable_scope(layer_name):
                 if 'input' in layer_name:
-                    input_size = conf['input_size']
-                    in_chs = conf['in_chs']
+                    input_size = self._config['input_size']
+                    in_chs = self._config['num_input_chs']
                     prev_layer = tf.placeholder(
                         dtype=tf.float32,
                         shape=(minibatch_size, input_size, input_size, in_chs),
@@ -181,17 +249,121 @@ class DCGAN:
                         ),
                     )
                 else:
+                    # XXX Just logits or plus sigmoid?
                     new_layer = tf.reshape(
-                        tf.sigmoid(pre_activation),
-                        [],
-                        name='output',
+                        pre_activation,
+                        (minibatch_size, 1),
+                        name='output_logits',
                     )
 
             # End of layer_name variable scope.
 
             prev_layer = new_layer
 
+    def _build_train_ops(self):
+        minibatch_size = self._config['minibatch_size']
+        input_size = self._config['input_size']
+        in_chs = self._config['num_input_chs']
+        input_shape = (minibatch_size, 1)
 
+        real_D_logits = tf.placeholder(
+            dtype=tf.float32,
+            shape=input_shape,
+            name='real_D_logits',
+        )
+
+        fake_D_logits = tf.placeholder(
+            dtype=tf.float32,
+            shape=input_shape,
+            name='fake_D_logits',
+        )
+
+        real_D_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf.ones_like(real_D_logits),
+                logits=real_D_logits,
+            )
+            name='real_D_loss',
+        )
+
+        fake_D_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf.zeros_like(fake_D_logits),
+                logits=fake_D_logits,
+            )
+            name='fake_D_loss',
+        )
+
+#        G_loss = -fake_D_loss 
+
+        G_loss = tf.reduce_mean(
+            tf.nn.sigmoid_cross_entropy_with_logits(
+                labels=tf.zeros_like(fake_D_logits),
+                logits=(-fake_D_logits),
+            )
+            name='G_loss',
+        )
+
+        # TODO: check hyperparameters.
+        adam = tf.train.AdamOptimizer(**self._config['adam'])
+
+        train_D_op = adam.minimize(
+            loss=(real_d_loss + fake_d_loss),
+            name='minimize_D_loss',
+        )
+
+        train_G_op = adam.minimize(
+            loss=G_loss,
+            name='minimize_G_loss',
+        )
+
+    def _load_data(self):
+        dataset_name=self._config['dataset_name']
+
+        if dataset_name = 'MNIST':
+            ((x_train, y_train),
+             (x_test, y_test)) = tf.contrib.keras.datasets.mnist.load_data()
+            self._mnist_data = {
+                'x_train': x_train,
+                'y_train': y_train,
+                'x_test': x_test,
+                'y_test': y_test,
+                'x_size': len(x_train),
+                'y_size': len(y_train),
+            }
+        else:
+            raise ValueError('Unknown dataset name: {}.'.format(dataset_name))
+
+    def get_samples_from_data(self, minibatch_size=1):
+        dataset_name=self._config['dataset_name']
+        
+        if dataset_name = 'MNIST':
+            samples = self._mnist_data['x_train'][
+                np.random.randint(
+                    low=0,
+                    high=self._mnist_data['x_size'],
+                    size=minibatch_size,
+                )
+            ]
+        else:
+            raise ValueError('Unknown dataset name: {}.'.format(dataset_name))
+
+        return samples
+
+    def generate_samples(self, minibatch_size=1):
+        feed_dict = {
+            self._tf_graph.get_tensor_by_name(
+                'generator/conv0_input/Z:0',
+            ): np.random.uniform(**self._config['generator_input'])
+        }
+
+        samples = self._tf_session.run(
+            fetches=self._tf_graph.get_tensor_by_name(
+                'generator/conv2_output/output:0',
+            ),
+            feed_dict=feed_dict,
+        )
+        
 
 def batch_normalization(
     input_tensor,
